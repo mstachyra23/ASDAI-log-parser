4cf01e7bb1229f8a50b8aa7434488c40c1211d81
Matthew <mstachyra@vicarioussurgical.com>
2023-01-18 17:00:44 -0500

    initial work with tkinter gui

--- __main__.py
+++ __main__.py
@@ -14,42 +14,54 @@ Last modified
 18 Jan 2023
 '''
 
-import csv        # used to capture the parsed log strings in csv format
-import argparse   # used to specify the parameters the script takes
-import subprocess # used to make UNIX grep call and capture stdout
-from utils import buildExtendedRegex
+import os             # used to make UNIX grep call and capture stdout
+import csv            # used to capture the parsed log strings in csv format
+import json       	  # used to convert the grepped string into JSON format
+import argparse       # used to specify the parameters the script takes
+import pathlib        # used to grab log file names for inputted path
 
-# command line
+from utils import buildExtendedRegex, renderDropdown
+
+
+# CLI
 parser = argparse.ArgumentParser(description = 'Return formatted csv, for \
 								 	inputted log, for inputted \
 									data strings to filter by.')
-parser.add_argument('-f', 
-					'--file', 
+parser.add_argument('-i', 
+					'--inpath', 
 					required=True, 
-					help="Path to the log file.")
+					help="Path to the log files.")
+parser.add_argument('-o', 
+					'--outfile', 
+					required=True, 
+					help="Desired path/filename for outputted csv.")
 parser.add_argument('-d', 
 					'--data', 
 					required=True,
 					nargs='+', 
 					help="Data string to query for, such as Topics, stored as \
 						string in a list, e.g., ['ROBOT_SUPPORT_STATE']")
-parser.add_argument('-t', 
-					'--time', 
-					help="The start and end time for which to filter the logs.")
 args = parser.parse_args()
 
 
-# grep to create new, smaller log
-_fname = args.file
-_data = args.data
-_time = args.time  # TODO  Make this work
-_grep_regex = buildExtendedRegex(_data)
-result = subprocess.run(['grep', '-iE', _grep_regex, _fname], 
-						 capture_output=True, 
-						 text=True)
+if __name__ == "__main__":
+	# UNIX grep to extract relevant part of the file
+	_in = args.inpath
+	_out = args.outfile
+	_data = args.data
+	_regex = buildExtendedRegex(_data)
+	_command = "grep -iE '{0}' '{1}' > '{2}'".format(_regex, _in, _out)
+	os.system(_command)
 
 
-# TODO  create csv
-# e.g.,
-# timestamp, topic, value
-# X, Y, [YA, YB, YC, ...]
+	# Tkinter GUI dropdown to select log files
+	_options = [file.name for file in pathlib.Path(_in).glob('*')]
+	renderDropdown(_options)
+
+
+
+
+	# TODO  create csv
+	# e.g.,
+	# timestamp, topic, value
+	# X, Y, [YA, YB, YC, ...]
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from tkinter import * # used to render drop down menu to select logs
+
+
 def buildExtendedRegex(data):
     return '|'.join(data)
 
@@ -6,3 +9,37 @@ def createCSV(path, data, dialect='excel'):
     with open(path, 'w', newline='') as csvfile:
         filewriter = csv.writer(csvfile)
         filewriter.writerow(data)
+
+
+def grabSelected():
+    '''Returns selected logs from the Tkinter GUI dropdown (listbox).
+    '''
+    return [listbox.get(i) for i in listbox.curselection()]
+
+
+def renderDropdown(options):
+    window = Tk()
+    window.geometry("750x350")
+    window.title("Log File Selection")
+    yscrollbar = Scrollbar(window)
+    yscrollbar.pack(side=RIGHT, fill=Y)
+    label = Label(window,
+                  text = "Select log files to parse:",
+                  font = ("Arial", 12), 
+                  padx = 10, pady = 10)
+    label.pack()
+    listbox = Listbox(window, selectmode = "multiple", 
+                   yscrollcommand = yscrollbar.set)
+
+    # Widget expands horizontally and 
+    # vertically by assigning both to
+    # fill option
+    listbox.pack(padx = 10, pady = 10,
+              expand = YES, fill = "both")
+    for i in range(len(options)):
+        listbox.insert(END, _options[i])
+        listbox.itemconfig(i, bg="white")
+
+    yscrollbar.config(command=listbox.yview)
+    selectButton = Button(window, text="Export log files", command=grabSelected)
+    window.mainloop()
